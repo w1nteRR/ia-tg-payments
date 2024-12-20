ee89dff28c4840b62b47efabc44e83e2f3b84216
w1nteRR <mykola796@gmail.com>
2024-12-20 03:26:55 +0200

    add get partial payment controller

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { container } from './di/payments.container'
 import { CheckPaymentController } from './presentation/controllers/check-payment.controller'
 import { ApiServer } from './presentation/ApiServer'
 import { ConfirmPaymentController } from './presentation/controllers/confirm-payment.controller'
+import { GetPartialPaymentController } from './presentation/controllers/get-partial-payment.controller'
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` })
 
@@ -11,9 +12,15 @@ const port = Number(process.env.PORT) || 3000
 
 const checkPaymentController = container.get(CheckPaymentController)
 const confirmPaymentController = container.get(ConfirmPaymentController)
+const getPartialPaymentController = container.get(GetPartialPaymentController)
 
 const start = async () => {
-  await ApiServer.run(port, checkPaymentController, confirmPaymentController)
+  await ApiServer.run(
+    port,
+    checkPaymentController,
+    confirmPaymentController,
+    getPartialPaymentController,
+  )
 }
 
 void start()
