c1227179833af3275ddf1a082df1f9ae22b118cd
w1nteRR <mykola796@gmail.com>
2024-12-20 03:25:47 +0200

    update payments repo

--- src/core/repositories/payments.repository.ts
+++ src/core/repositories/payments.repository.ts
@@ -1,7 +1,10 @@
 import { CheckPaymentDto } from './dto/check-payment.dto'
 import { ConfirmPaymentDto } from './dto/confirm-payment.dto'
+import { Payment } from '../domain/payment'
+import { GetPaymentDto } from './dto/get-payment.dto'
 
 export interface IPaymentsRepository {
   checkPayment: (dto: CheckPaymentDto) => Promise<boolean>
   confirmPayment: (dto: ConfirmPaymentDto) => Promise<void>
+  getPartialPayment: (dto: GetPaymentDto) => Promise<Partial<Payment> | null>
 }
