f58a60eeffc46833b5ab5ed15ff82c8821581992
w1nteRR <mykola796@gmail.com>
2024-12-21 03:55:18 +0200

    update payments repository

--- src/core/repositories/payments.repository.ts
+++ src/core/repositories/payments.repository.ts
@@ -2,9 +2,15 @@ import { CheckPaymentDto } from './dto/check-payment.dto'
 import { ConfirmPaymentDto } from './dto/confirm-payment.dto'
 import { Payment } from '../domain/payment'
 import { GetPaymentDto } from './dto/get-payment.dto'
+import { FindPaymentDto } from './dto/find-payment-by-field.dto'
+import { UpdatePaymentDto } from './dto/update-payment.dto'
+import { DeletePaymentDto } from './dto/delete-payment.dto'
 
 export interface IPaymentsRepository {
   checkPayment: (dto: CheckPaymentDto) => Promise<boolean>
   confirmPayment: (dto: ConfirmPaymentDto) => Promise<void>
   getPartialPayment: (dto: GetPaymentDto) => Promise<Partial<Payment> | null>
+  findPayment: (dto: FindPaymentDto) => Promise<Payment | null>
+  updatePayment: (dto: UpdatePaymentDto) => Promise<void>
+  deletePayment: (dto: DeletePaymentDto) => Promise<void>
 }
